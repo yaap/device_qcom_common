3a3878a323e19bf63e656a38dea7b9c0b749f160
John Galt <johngaltfirstrun@gmail.com>
2025-04-10 08:55:51 -0400

    wfd: also shim wfdservice

--- system/extract-files.sh
+++ system/extract-files.sh
@@ -97,7 +97,7 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "android.hardware.common-V2-ndk_platform.so" "android.hardware.common-V2-ndk.so" "${2}"
             ;;
-        system_ext/bin/wfdservice64)
+        system_ext/bin/wfdservice64 | system_ext/bin/wfdservice)
             [ "$2" = "" ] && return 0
             grep -q "libwfdservice_shim.so" "${2}" || "${PATCHELF}" --add-needed "libwfdservice_shim.so" "${2}"
             ;;
